8bd40b1025bb98436ae622ce359a0bcce49549dd
Ryan LaBouve <imryanlabouve@gmail.com>
2016-04-09 23:04:22 -0500

    Massive spike to v 1

--- index.js
+++ index.js
@@ -1,27 +1,57 @@
 #!/usr/bin/env node
 var util = require('util');
 var exec = require('child_process').exec;
-var stdToObj = require('./lib/std-to-obj');
+var stdoutToObj = require('./lib/std-to-obj');
+var killAProcess = require('./lib/kill-a-process');
+var _ = require('lodash');
+var debug = require('debug')('main');
 
 var child;
 
 var program = require('commander')
   .version('1.0.0')
-  .option('-k, --kill', 'Kill this process after it\'s found.')
+  .option('-k, --kill', 'Kill this process after it\'s found.');
+
+var portOfInterest = _.get(process.argv, 2);
+
+if (!portOfInterest) {
+  throw "Must Provide Port";
+}
 
 program.on('--help', function() {
-  console.log('Example: `misport 3000`');
-  console.log('>>> info about what\'s running');
+  debug('Example: `misport 3000`');
+  debug('>>> info about what\'s running');
 });
 
-child = exec("lsof -i :4205", function(err, stdout, stderr) {
-  stdToObj(stdout);
-  // util.print('stdout: ' + stdout);
-  // util.print('stderr: ' + stderr);
+function runLsof(port) {
+  var portLookup = ["lsof -i :", port].join('');
+  debug("Running lookup: ", portLookup);
+
+  child = exec(portLookup, function(err, stdout, stderr) {
+    var lsofResults = stdoutToObj(stdout, /(LISTEN)/);
+    debug('~> Found ' + lsofResults.length + ' result(s)');
+
+    var activeProcess = _.find(lsofResults, function(p) {
+      return _.get(p, '_active') === true;
+    });
+
+    debug(activeProcess);
+
+    killAProcess(activeProcess);
+  });
+}
+
+function killAProcess(activeProcess) {
+  var activePID = _.get(activeProcess, 'PID');
+  debug('~*~*~*~', activePID);
+  var killCommand = ["kill -9", activePID].join(' ');
+  debug(killCommand);
+  // exec(killCommand, function(err,stdout, stderr) {
+  //   if (err) throw err;
+  //   console.log("misport success");
+  // });
   //
-  if (err !== null ) {
-    console.log('exec error: ', err);
-  }
-});
+}
 
+runLsof(portOfInterest);
 program.parse(process.argv);
--- tests/lib/std-to-obj-test.js
+++ tests/lib/std-to-obj-test.js
@@ -4,19 +4,19 @@ var stdToObj = require('../../lib/std-to-obj');
 test('stdToObj', function (t) {
   t.plan(6);
 
-  noOutput = '';
+  var noOutput = '';
   t.deepEqual(
     stdToObj(noOutput),
     {},
     'No input should yield empty object');
 
-  onlyFirstRow = 'TITLE1 TITLE2 TITLE3 TITLE4';
+  var onlyFirstRow = 'TITLE1 TITLE2 TITLE3 TITLE4';
   t.deepEqual(
     stdToObj(onlyFirstRow),
     [],
     'Only passing titles should result in empty object');
 
-  happyPath= 'TITLE1 TITLE2 TITLE3 TITLE4\nval1 val2 val3 val4';
+  var happyPath= 'TITLE1 TITLE2 TITLE3 TITLE4\nval1 val2 val3 val4';
   t.deepEqual(
     stdToObj(happyPath),
     [
@@ -30,7 +30,7 @@ test('stdToObj', function (t) {
     'Happy path works'
   );
 
-  tooManyTitles= 'TITLE1 TITLE2 TITLE3 TITLE4\nval1 val2 val3';
+  var tooManyTitles= 'TITLE1 TITLE2 TITLE3 TITLE4\nval1 val2 val3';
   t.deepEqual(
     stdToObj(tooManyTitles),
     [
@@ -44,7 +44,7 @@ test('stdToObj', function (t) {
     'Missing properties should be undefined'
   );
 
-  tooManyValues= 'TITLE1 TITLE2 TITLE3\nval1 val2 val3 val4';
+  var tooManyValues= 'TITLE1 TITLE2 TITLE3\nval1 val2 val3 val4';
   t.deepEqual(
     stdToObj(tooManyValues),
     [
@@ -57,7 +57,7 @@ test('stdToObj', function (t) {
     'Extra properties should be thrown away'
   );
 
-  tooManyValues= 'TITLE1 TITLE2 TITLE3 TITLE4\nval1 val2 val3 val4\n';
+  var tooManyValues= 'TITLE1 TITLE2 TITLE3 TITLE4\nval1 val2 val3 val4\n';
   t.deepEqual(
     stdToObj(tooManyValues),
     [
@@ -70,4 +70,27 @@ test('stdToObj', function (t) {
     ],
     'Extra newlines should not result in another row'
   );
+});
+
+test('stdToObj marked as active', function (t) {
+  t.plan(3);
+
+  var happyPath = 'TITLE1 TITLE2 TITLE3\nval1 val2 val3 (LISTEN)';
+  var happyPathRes = stdToObj(happyPath, /(LISTEN)/);
+  t.equal(
+    happyPathRes[0]['_active'],
+    true,
+    'found active marker and marked active');
+
+  var invalidPath = 'TITLE1 TITLE2 TITLE3\nval1 val2 val3';
+  t.equal(
+    stdToObj(invalidPath, /(LISTEN)/)[0]['_active'],
+    false,
+    'didn\'t find active marker and marked active');
+
+  var undefinedPath = 'TITLE1 TITLE2 TITLE3\nval1 val2 val3';
+  t.equal(
+    stdToObj(undefinedPath)['_active'],
+    undefined,
+    'didn\'t pass active marker, so should be undefined');
 });
